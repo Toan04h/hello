eae7077aa196f31b850831d8c33498a6c9eac15a
Toan04h <mhuynh10@husker.unl.edu>
2023-08-24 20:43:01 -0500

    added my major to the output

--- hello.c
+++ hello.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-    //Print out my name. 
+    //Print out my name.
   printf("Toan Huynh\n");
+  printf("My major is Computer Science!\n);
 
   return 0;
 }
